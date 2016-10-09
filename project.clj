d895edcc573e6d06a9aec1681d1a7371ae0400d7
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2016-10-09 22:16:43 +0100

    adding codox documentation plugin

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot clj-game-of-life.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-codox "0.10.1"]])
